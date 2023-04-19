089d706124629079b6e3b3056f7fca34a948efa1
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-04-19 16:46:20 +0200

    milestone 1 done

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php 
+
+$lengthPassword = isset($_GET['lunghezza']) ? $_GET['lunghezza'] : '';
+
+function randomPassword($lengthPass) {
+
+  $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*(){}[],./?';
+
+  return substr(str_shuffle($permitted_chars), 0, $lengthPass);
+
+};
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -7,6 +22,14 @@
   <title>Password generator</title>
 </head>
 <body>
-  
+  <form action="index.php" >
+    <input type="number" min="4" max="18" name="lunghezza" placeholder="Scegli un numero tra 4 e 18">
+    <button type="submit">Invia</button>
+  </form>
+
+  <?php if($lengthPassword != null){
+    echo randomPassword($lengthPassword);
+  }
+   ?>
 </body>
 </html>
